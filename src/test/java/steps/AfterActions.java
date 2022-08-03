e23b1ca51b6a8a9d537962d6da7ef112636c82e5
Dheeraj Garg <dheeraj@vinsol.com>
2022-08-03 18:32:03 +0530

    changes

--- src/test/java/steps/AfterActions.java
+++ src/test/java/steps/AfterActions.java
@@ -9,21 +9,21 @@ import cucumber.api.java.After;
 import utils.SeleniumDriver;
 
 public class AfterActions {
-	
-    @After
-    
-    public static void tearDown(Scenario scenario) {
-    	
-    	WebDriver driver=SeleniumDriver.getDriver();
-    	System.out.println(scenario.isFailed());
-    	 if (scenario.isFailed()) {
-             byte[] screenshotBytes = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
-             scenario.embed(screenshotBytes, "image/png");
-          
-         }
-    	 
-    SeleniumDriver.tearDown();
-    }
-	
+
+	@After
+
+	public static void tearDown(Scenario scenario) {
+
+		WebDriver driver = SeleniumDriver.getDriver();
+		System.out.println(scenario.isFailed());
+		if (scenario.isFailed()) {
+			byte[] screenshotBytes = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+			scenario.embed(screenshotBytes, "image/png");
+
+		}
+
+		System.out.println("Dheeraj");
+		SeleniumDriver.tearDown();
+	}
 
 }
--- src/test/java/steps/LoginTest.java
+++ src/test/java/steps/LoginTest.java
@@ -37,51 +37,46 @@ public class LoginTest {
 
 	@When("^user clicks on the Sign in google$")
 	public void user_clicks_on_the_Sign_in_google() throws Throwable {
-		
-		
+
 		SeleniumDriver.getDriver().manage().deleteCookieNamed("_si2_session");
-		
-		//socialImprintsLoginActions.click_on_sign_in_with_google();
+
+		// socialImprintsLoginActions.click_on_sign_in_with_google();
 	}
 
 	@Then("^user entered username \"([^\"]*)\"$")
 	public void user_entered_username(String arg1) throws Throwable {
-		
-		SeleniumDriver.getDriver().manage().addCookie(new Cookie("_si2_session","WjFKWGdoenJQTFh2RUpWdjZYTSt1c09UOHRMYkRpamJRZVNmNkF5a3JkOEJhbHpqcmsycjdXMkdQRkRidm5sWmpzU2FoM1RzY2UwTHBLZU9Nc25GRW1DeGZQR04ycXRET09WSENCdDc4S2cvT1lwMTVBTlorS3R3bWY4YkxVL2EzcnBYakc0Z1JPNFY0eXVEeHNwWkYvYTdGKzgyLzh1TjB3b0JGakJYK0hQMFRQSk50ckdESE1SUXg2TEZpaE9yTHJkWnFtNWxHN3FkWmNJNFlJQWVWdz09LS1SV05kZWNsWnI1Z2hIYUwxdkdNKzBnPT0%3D--37f6368ebb1a19e2663121a8c3cbccaf55014524"));
-		
+
+		SeleniumDriver.getDriver().manage().addCookie(new Cookie("_si2_session",
+				"WjFKWGdoenJQTFh2RUpWdjZYTSt1c09UOHRMYkRpamJRZVNmNkF5a3JkOEJhbHpqcmsycjdXMkdQRkRidm5sWmpzU2FoM1RzY2UwTHBLZU9Nc25GRW1DeGZQR04ycXRET09WSENCdDc4S2cvT1lwMTVBTlorS3R3bWY4YkxVL2EzcnBYakc0Z1JPNFY0eXVEeHNwWkYvYTdGKzgyLzh1TjB3b0JGakJYK0hQMFRQSk50ckdESE1SUXg2TEZpaE9yTHJkWnFtNWxHN3FkWmNJNFlJQWVWdz09LS1SV05kZWNsWnI1Z2hIYUwxdkdNKzBnPT0%3D--37f6368ebb1a19e2663121a8c3cbccaf55014524"));
+
 		/*
-		gmailEmailIDActions.enter_email_id();
-		gmailEmailIDActions.click_on_next();
-		*/
-		
+		 * gmailEmailIDActions.enter_email_id(); gmailEmailIDActions.click_on_next();
+		 */
+
 	}
 
 	@Then("^user entered password \"([^\"]*)\"$")
 	public void user_entered_password(String arg1) throws Throwable {
-		
+
 		SeleniumDriver.openPage("https://vinsol:v1ns0l@staging.dev.socialimprints.com/admin/sign_in");
 		/*
-		gmailPasswordActions.enter_password();
-		Thread.sleep(10000);
-		gmailPasswordActions.click_on_submit();
-		Thread.sleep(100000);
-		*/
-		
+		 * gmailPasswordActions.enter_password(); Thread.sleep(10000);
+		 * gmailPasswordActions.click_on_submit(); Thread.sleep(100000);
+		 */
+
 	}
 
 	@Then("^user logged successfully$")
 	public void user_logged_successfully() throws Throwable {
-		//Thread.sleep(10000);
+		// Thread.sleep(10000);
 		String actualtitle = SeleniumDriver.getDriver().getTitle();
-		//String actualtitle = SeleniumDriver.getDriver().findElement(By.xpath("/html/body/div[7]/div[20]/div/article/div/div/span[1]")).getText();
-		Assert.assertEquals("Social Imprints Test", actualtitle);
-		
-		//Test cases will executed when i pushed my code to github. Through Github actions.
+		// String actualtitle =
+		// SeleniumDriver.getDriver().findElement(By.xpath("/html/body/div[7]/div[20]/div/article/div/div/span[1]")).getText();
+		Assert.assertEquals("Social Imprints", actualtitle);
+
+		// Test cases will executed when i pushed my code to github. Through Github
+		// actions.
 
 	}
-	
-		 
-	}
-	
-	
 
+}
