86b4a0c8672ad0e36ee04d48ea01b06862eb1b8c
Dheeraj Garg <dheeraj@vinsol.com>
2022-09-02 16:14:48 +0530

    two features present

--- src/main/java/utils/SeleniumDriver.java
+++ src/main/java/utils/SeleniumDriver.java
@@ -28,34 +28,72 @@ public class SeleniumDriver {
 
 		// Updated comment just to trigger build automatically when i merge my changes
 		// to github...
+
 		// Proxy proxy = new Proxy();
+
 		// proxy.setHttpProxy("198.59.191.234:8080");
-		// String PROXY = "198.59.191.234:8080";
-		ChromeOptions options = new ChromeOptions();
-		options.addArguments("--incognito");
-		// options.addArguments("--proxy-server=198.59.191.234:8080");
-		// options.addArguments("--proxy-server=%s", PROXY);
-		// options.setCapability("proxy", proxy);
+
+		// proxy.setHttpProxy("23.94.98.201:8080");
+
+		/*
+		 * ChromeOptions chromeOptions = new ChromeOptions();
+		 * 
+		 * Proxy proxy = new Proxy(); proxy.setAutodetect(false);
+		 * 
+		 * proxy.setHttpProxy("159.89.91.243:7497");
+		 * 
+		 * // proxy.setSslProxy("40.122.238.76:8080"); proxy.setNoProxy("no_proxy-var");
+		 * 
+		 * chromeOptions.setCapability("proxy", proxy);
+		 */
+
 		WebDriverManager.chromedriver().setup();
-		driver = new ChromeDriver(options);
-		//driver = new ChromeDriver();
 
-		Map<String, Object> coordinatesMap = new HashMap<String, Object>();
-		coordinatesMap.put("latitude", 34.052235);
-		coordinatesMap.put("longitude", -118.243683);
-		coordinatesMap.put("accuracy", 1);
+		// driver = new ChromeDriver(chromeOptions);
 
-		((ChromeDriver) driver).executeCdpCommand("Emulation.setGeolocationOverride", coordinatesMap);
+		// driver.get("https://admin:admin@staging-frontend.snackmagic.com");
+
+		// String PROXY = "23.94.98.201:8080";
+
+		// ChromeOptions options = new ChromeOptions();
+
+		// options.addArguments("--incognito");
+
+		// options.addArguments("--proxy-server=198.59.191.234:8080");
+		// options.addArguments("--proxy-server=http://" + PROXY);
+
+		// options.addArguments("--proxy-server=%s", PROXY);
+
+		// options.setCapability("proxy", proxy);
+
+		// WebDriverManager.chromedriver().setup();
+
+		// driver = new ChromeDriver(options);
+
+		driver = new ChromeDriver();
+
+		/*
+		 * Map<String, Object> coordinatesMap = new HashMap<String, Object>();
+		 * coordinatesMap.put("latitude", 34.052235); coordinatesMap.put("longitude",
+		 * -118.243683); coordinatesMap.put("accuracy", 1);
+		 * 
+		 * ((ChromeDriver) driver).executeCdpCommand("Emulation.setGeolocationOverride",
+		 * coordinatesMap);
+		 */
+
 		// WebDriverManager.firefoxdriver().setup();
 		// driver = new FirefoxDriver();
 
 		driver.manage().window().maximize();
 
-		waitDriver = new WebDriverWait(driver, TIMEOUT);
-		driver.manage().timeouts().implicitlyWait(TIMEOUT, TimeUnit.SECONDS);
-		driver.manage().timeouts().pageLoadTimeout(PAGE_LOAD_TIMEOUT, TimeUnit.SECONDS);
-		String window = driver.getWindowHandle();
-		System.out.println("Window ->" + window);
+		/*
+		 * waitDriver = new WebDriverWait(driver, TIMEOUT);
+		 * driver.manage().timeouts().implicitlyWait(TIMEOUT, TimeUnit.SECONDS);
+		 * driver.manage().timeouts().pageLoadTimeout(PAGE_LOAD_TIMEOUT,
+		 * TimeUnit.SECONDS); String window = driver.getWindowHandle();
+		 * System.out.println("Window ->" + window);
+		 */
+
 	}
 
 	public static void openPage(String url) {
